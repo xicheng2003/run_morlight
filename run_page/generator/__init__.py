0edf1a00309168f041c7ef9da34fdca531574eef
Xicheng2003 <xicheng.lin@foxmail.com>
2026-03-23 16:11:19 +0800

    fix ci black formatting

--- run_page/generator/__init__.py
+++ run_page/generator/__init__.py
@@ -11,7 +11,6 @@ from polyline_processor import filter_out
 from .db import Activity, init_db, update_or_create_activity
 from synced_data_file_logger import save_synced_data_file_list
 
-
 IGNORE_BEFORE_SAVING = os.getenv("IGNORE_BEFORE_SAVING", False)
 
 
